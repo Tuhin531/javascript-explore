8cd3ec3898858f5b4714979f79d26922750de330
Tuhin531 <mdtuhinhossain153@gmail.com>
2024-02-11 10:44:14 +0100

    add new program

---
+++ split.js
@@ -0,0 +1,7 @@
+const lyrics = "tumi bondhu kala pakhi. ami jeno ki.bosonto kal e tumi bolta pai ne.";
+const parts = lyrics.split(' ');
+const sentances = lyrics.split('.')
+const chars = lyrics.split('');
+console.log(parts);
+console.log(sentances);
+console.log(chars);
